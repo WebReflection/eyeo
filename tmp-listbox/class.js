df14a8a72504ba35edcf5006e987a0dfccfbb9b3
Andrea Giammarchi <a.giammarchi@eyeo.com>
2018-02-21 16:50:00 +0000

    latest progress

--- tmp-listbox/class.js
+++ tmp-listbox/class.js
@@ -1,3 +1,20 @@
+const KeyCode = {
+  "BACKSPACE": 8,
+  "TAB": 9,
+  "RETURN": 13,
+  "ESC": 27,
+  "SPACE": 32,
+  "PAGE_UP": 33,
+  "PAGE_DOWN": 34,
+  "END": 35,
+  "HOME": 36,
+  "LEFT": 37,
+  "UP": 38,
+  "RIGHT": 39,
+  "DOWN": 40,
+  "DELETE": 46
+};
+
 class Listbox {
   /**
    * @param {*} details
@@ -13,21 +30,26 @@ class Listbox {
     this.id = 'listbox-' + ('' + Math.random()).replace(/\D/, '');
     this.items = [];
     this.nodes = [];
-    this.selectedItem = -1;
+    this.hoveredIndex = 0;
     this.target = typeof details.target === 'string' ?
                   document.querySelector(details.target) : details.target;
-
     this.target.classList.add('listbox');
+    this.text = {
+      // details.label can be either an object or a string
+      default: details.label.default || details.label,
+      expanded: details.label.expanded || details.label
+    };
 
     // a button is used as label to capture the initial focus
     this.label = this.target.appendChild(document.createElement('button'));
     this.label.id = this.id + '-label';
-    this.label.textContent = details.label;
+    this.label.textContent = this.text.default;
     this.label.setAttribute('aria-haspopup', this.id + '-popup');
-    this.label.setAttribute('aria-expanded', 'false');
+    this.label.setAttribute('aria-expanded', false);
 
     this.label.addEventListener('blur', this);
     this.label.addEventListener('focus', this);
+    this.label.addEventListener('keydown', this);
 
     // the list of items is shown through a popup
     this.popup = this.target.appendChild(document.createElement('ul'));
@@ -39,7 +61,14 @@ class Listbox {
     for (const item of details.items)
       this.add(item);
 
+    // if no item was selected
+    if (this.hoveredIndex === 0) {
+      // hover at least the first one
+      this.hover(details.items[0]);
+    }
+
     this.popup.addEventListener('click', this);
+    this.popup.addEventListener('mouseover', this);
 
     // extra properties used to handle the listbox state
     this._blurTimer = 0;
@@ -50,19 +79,20 @@ class Listbox {
   }
 
   onblur(event) {
-    this._blurTimer = setTimeout(addHiddenAttribute, 400, this.popup);
+    this._blurTimer = setTimeout(hidePopup.bind(this), 400);
   }
 
   onclick(event) {
     event.preventDefault();
     clearTimeout(this._blurTimer);
     const el = event.target.closest('[role="option"]:not([aria-disabled="true"])');
-    addHiddenAttribute(this.popup);
+    hidePopup.call(this);
     if (el) {
       const item = this.items[this.nodes.indexOf(el)];
       const selected = !JSON.parse(el.getAttribute('aria-selected'));
-      this.setInfo(item, {selected});
+      this.setInfo(item, {selected, disabled: selected});
       el.setAttribute('aria-selected', selected);
+      el.setAttribute('aria-disabled', selected);
     }
   }
 
@@ -71,6 +101,43 @@ class Listbox {
     clearTimeout(this._blurTimer);
     // show the popup
     this.popup.removeAttribute('hidden');
+    this.label.setAttribute('aria-expanded', true);
+    this.label.textContent = this.text.expanded;
+  }
+
+  onkeydown(event) {
+    event.preventDefault();
+    const index = this.hoveredIndex;
+    const key = event.which || event.keyCode;
+    switch (key) {
+      case KeyCode.BACKSPACE:
+      case KeyCode.DELETE:
+        break;
+      /* both SPACE, RETURN and ESC hide and blur */
+      case KeyCode.RETURN:
+      case KeyCode.SPACE:
+        /* space also trigger a click event */
+        const evt = new CustomEvent('click', {bubbles: true});
+        this.nodes[index].dispatchEvent(evt);
+        /* eslint: fall through */
+      case KeyCode.ESC:
+        hidePopup.call(this);
+        this.label.blur();
+        break;
+      case KeyCode.UP:
+        const prev = findNext.call(this, index, -1);
+        if (prev) this.hover(prev);
+        break;
+      case KeyCode.DOWN:
+        const next = findNext.call(this, index, 1);
+        if (next) this.hover(next);
+        break;
+    }
+  }
+
+  onmouseover(event) {
+    const el = event.target.closest('[role="option"]');
+    if (el) this.hover(this.items[this.nodes.indexOf(el)]);
   }
 
   add(item) {
@@ -91,12 +158,30 @@ class Listbox {
       li.id = this.id + '-' + index;
       const info = this.getInfo(item);
       li.setAttribute('role', 'option');
-      li.setAttribute('aria-disabled', info.disabled);
+      li.setAttribute('aria-disabled', info.selected);
       li.setAttribute('aria-selected', info.selected);
       li.textContent = info.value;
       if (info.selected) {
-        this.selectedItem = index;
-        this.target.setAttribute('aria-activedescendant', li.id);
+        this.hover(item);
+      }
+    }
+  }
+
+  hover(item) {
+    this.nodes[this.hoveredIndex].classList.remove('hover');
+    const index = this.items.indexOf(item);
+    const element = this.nodes[index];
+    element.classList.add('hover');
+    this.hoveredIndex = index;
+    this.popup.setAttribute('aria-activedescendant', element.id);
+    if (this.popup.scrollHeight > this.popup.clientHeight) {
+      const scrollBottom = this.popup.clientHeight + this.popup.scrollTop;
+      const elementBottom = element.offsetTop + element.offsetHeight;
+      if (elementBottom > scrollBottom) {
+        this.popup.scrollTop = elementBottom - this.popup.clientHeight;
+      }
+      else if (element.offsetTop < this.popup.scrollTop) {
+        this.popup.scrollTop = element.offsetTop;
       }
     }
   }
@@ -110,8 +195,27 @@ class Listbox {
   }
 }
 
-function addHiddenAttribute(element) {
-  element.setAttribute('hidden', 'hidden');
+function hidePopup() {
+  this.popup.setAttribute('hidden', 'hidden');
+  this.label.setAttribute('aria-expanded', false);
+  this.label.textContent = this.text.default;
+}
+
+function findNext(index, increment) {
+  let next = index;
+  do {
+    next += increment;
+    if (increment < 0 && next < 0)
+      next = this.items.length - 1;
+    else if (increment > 0 && next >= this.items.length)
+      next = 0;
+  } while(
+    // avoid infinite loops
+    next !== index &&
+    // keep doing this while current item is disabled
+    this.getInfo(this.items[next]).disabled
+  );
+  return next === index ? null : this.items[next];
 }
 
 
@@ -120,11 +224,15 @@ document.addEventListener(
   () => {
     new Listbox({
       target: '#testing',
-      label: 'Select your language',
+      label: {
+        default: '+ Add lanugages',
+        expanded: 'Select your language'
+      },
       getInfo(item) {
+        const selected = 'selected' in item ? item.selected : !item.disabled;
         return {
-          disabled: Math.random() < .2,
-          selected: !item.disabled,
+          disabled: selected,
+          selected: selected,
           get value() {
             return item.value;
           }
