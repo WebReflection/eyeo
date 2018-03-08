f167c8f56ed462ed4ddc25c0c4ede7d3db89283f
Andrea Giammarchi <a.giammarchi@eyeo.com>
2018-03-08 19:20:09 +0100

    added prototype

--- bubble-ui/class.js
+++ bubble-ui/class.js
@@ -1,8 +1,36 @@
 class BubbleUI extends BaseComponent {
   constructor(details) {
     super(details);
+    this.target
+        .querySelector('[data-component=switcher]')
+        .addEventListener('change', this);
   }
-  update() {}
+  handleEvent(event) {
+    this['on' + event.type](event);
+  }
+  onchange(event) {
+    const main = this.target.querySelector('main');
+    const showingOptions = !main.classList.contains('options');
+    dropNegativeTabIndex(
+      showingOptions,
+      main.querySelectorAll('.switcher-options button')
+    );
+    dropNegativeTabIndex(
+      !showingOptions,
+      main.querySelectorAll('.details button')
+    );
+    main.classList.toggle('options');
+  }
+}
+
+function dropNegativeTabIndex(dropIt, list) {
+  for (const element of list) {
+    if (dropIt) {
+      element.removeAttribute('tabindex');
+    } else {
+      element.setAttribute('tabindex', '-1');
+    }
+  }
 }
 
 document.addEventListener(
